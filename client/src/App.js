1e9231c0a08ea8623d3d5add0269f49cb24ad995
Chu gyo yoon <ksn03012@naver.com>
2021-01-19 18:23:14 +0900

    Third

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import CustomerAdd from './components/CustomerAdd';
 
 
 const styles = theme => ({
@@ -26,15 +27,31 @@ const styles = theme => ({
 })
 
 class App extends React.Component {
-  state = {
-    customers: "",
-    completed: 0
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+    this.stateRefresh = this.stateRefresh.bind(this);
   }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    })
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
   componentDidMount() {
     this.timer = setInterval(this.progress, 800);
-    // this.callApi()
-    //   .then(res => this.setState({ customers: res }))
-    //   .catch(err => console.log(err));
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
   }
   callApi = async () => {
     const response = await fetch('/api/customers');
@@ -51,40 +68,51 @@ class App extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>id</TableCell>
-              <TableCell>프로필</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {
-              this.state.customers ? this.state.customers.map(c => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                )
-              }) : <TableRow><TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}></CircularProgress></TableCell></TableRow>
-            }
-          </TableBody>
-        </Table>
-      </Paper>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                <TableCell>프로필</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                this.state.customers ? this.state.customers.map(c => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  )
+                }) : <TableRow><TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}></CircularProgress></TableCell></TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh} />
+      </div>
     );
   }
 }
 
 export default withStyles(styles)(App);
+
+
+
+
+
+//현재 상황!!! ㅅㅂ App컴포넌트에서 props로 stateRefesh 함수를 CustomerAdd컴포넌트한테 넘겨줬는데 CustomerAdd에서 stateRefresh함수를 인식을 못함. 콘솔로그 찍어 봤는데 존나 웃긴건 함수가 실행은 되긴 함.  
+//문제는 reset이 안된다는 거임 ㅅㅂ 왜 안되는거임 ㅅㅂ,,,,!!!
+//그거 말고는 다 잘됨.
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,43 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'http://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '961222',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': 'http://placeimg.com/64/64/2',
-            'name': '이순신',
-            'birthday': '960515',
-            'gender': '남자',
-            'job': '장군'
-        },
-        {
-            'id': 3,
-            'image': 'http://placeimg.com/64/64/3',
-            'name': '이영애',
-            'birthday': '941107',
-            'gender': '여자',
-            'job': '직장인'
-        }
-    ]);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
 });
+connection.connect();
+
+const multer = require('multer');
+const upload = multer({ dest: './upload' });
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+        'SELECT * FROM CUSTOMER',
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+});
+
+app.use('/image', express.static('./upload'));
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params, (err, rows, fields) => {
+        res.send(rows);
+    })
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
